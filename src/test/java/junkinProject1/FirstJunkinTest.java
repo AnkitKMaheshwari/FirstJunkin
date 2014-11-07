fbcc83666c5c4ccbe2916391672e0d7ff6e7921f
Ankit <ankit.maheshwari@gspann.com>
2014-11-07 11:24:05 +0530

    first commit

---
+++ src/test/java/junkinProject1/FirstJunkinTest.java
@@ -0,0 +1,16 @@
+package junkinProject1;
+
+import org.testng.annotations.Test;
+
+public class FirstJunkinTest {
+	
+	@Test
+	public void firstTest()
+	{
+		System.out.println("Hello junkins");
+	}
+	
+	
+	
+
+}
